80b2a632006941b35d9585305a040cf4da968728
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-07-09 19:02:38 -0400

    created tables in schema

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 const inquirer = require('inquirer')
-const mysql2 = require('mysql2')
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'company_vault'
+});
 
 
-inquirer.prompt = [{
-    name: 'view-departments',
-    list: []
-}]
+
+
+function startApplication() {
+    // Display menu options to the user
+    console.log("Welcome to the Employee Management System!");
+    console.log("Please select an option:");
+    console.log("1. View all departments");
+    console.log("2. View all roles");
+    console.log("3. View all employees");
+    console.log("4. Add a department");
+    console.log("5. Add a role");
+    console.log("6. Add an employee");
+    console.log("7. Update an employee role");
+  
+    // Wait for user input
+    // Call corresponding functions based on user input
+  };
--- schema.sql
+++ schema.sql
@@ -1,6 +1,28 @@
 DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_db;
+CREATE DATABASE company_vault;
 
-USE company_db;
+USE company_vault;
 
-CREATE TABLE department;
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT, 
+     name VARCHAR(100) NOT NULL,
+     PRIMARY KEY (id)
+);
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT ,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT AUTO_INCREMENT,
+    manager_id INT AUTO_INCREMENT
+    FOREIGN KEY(role_id) REFERENCES role(id)
+);
