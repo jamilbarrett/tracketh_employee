9f22b2745f5141bafd3505b9fe167e809c590dc1
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-07-09 20:54:19 -0400

    re added sql files

--- index.js
+++ index.js
@@ -5,24 +5,14 @@ const connection = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
     password: '',
-    database: 'company_vault'
+    database: 'the_legion'
 });
 
+inquirer.prompt = [{
+    input: 'list',
+    name: 'overview',
+    message: 'Welcome to the LEGIONs management interface , we are everywhere & nowhere. Proceed',
 
 
+}];
 
-function startApplication() {
-    // Display menu options to the user
-    console.log("Welcome to the Employee Management System!");
-    console.log("Please select an option:");
-    console.log("1. View all departments");
-    console.log("2. View all roles");
-    console.log("3. View all employees");
-    console.log("4. Add a department");
-    console.log("5. Add a role");
-    console.log("6. Add an employee");
-    console.log("7. Update an employee role");
-  
-    // Wait for user input
-    // Call corresponding functions based on user input
-  };
--- schema.sql
+++ schema.sql
@@ -1,28 +1,29 @@
-DROP DATABASE IF EXISTS company_db;
-CREATE DATABASE company_vault;
+DROP DATABASE IF EXISTS the_legion;
+CREATE DATABASE the_legion;
 
-USE company_vault;
+USE DATABASE the_legion;
 
-CREATE TABLE department (
-    id INT NOT NULL AUTO_INCREMENT, 
-     name VARCHAR(100) NOT NULL,
-     PRIMARY KEY (id)
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30)
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL,
-    department_id INT ,
+    title VARCHAR(30),
+    salaray DECIMAL,
+    department_id INT 
+    PRIMARY KEY (id)
     FOREIGN KEY (department_id) REFERENCES department(id)
-
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT AUTO_INCREMENT,
-    manager_id INT AUTO_INCREMENT
-    FOREIGN KEY(role_id) REFERENCES role(id)
+    manager_id INT AUTO_INCREMENT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
